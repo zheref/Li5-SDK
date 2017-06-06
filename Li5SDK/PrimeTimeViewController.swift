02c11102baf11f287c779b733fa66af6622baa9a
Sergio Daniel Lozano <zheref@icloud.com>
2017-06-06 10:24:34 -0500

    Add new PrimeTimeViewController

---
+++ Li5SDK/PrimeTimeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PrimeTimeViewController.swift
+//  li5
+//
+//  Created by Sergio Daniel L. García on 6/6/17.
+//  Copyright © 2017 Li5, Inc. All rights reserved.
+//
+
+import Foundation
+
+class PrimeTimeVC : Li5UIPageViewController {
+    
+    init(withDataSource source: PrimeTimeViewControllerDataSource) {
+        
+    }
+    
+//    - (instancetype)initWithDataSource:(PrimeTimeViewControllerDataSource*)source;
+//    
+//    - (void)setStartIndex:(NSInteger)idx;
+//    
+//    @property (nonatomic, retain) ExploreProductInteractor *interactor;
+    
+}
